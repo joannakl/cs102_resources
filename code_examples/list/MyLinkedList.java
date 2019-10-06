75b70e25100db3332078da73a24b48ba61e5967c
Joanna Kl <joannakl@cs.nyu.edu>
2019-10-06 16:06:21 -0400

    update the example

--- code_examples/list/MyLinkedList.java
+++ code_examples/list/MyLinkedList.java
@@ -57,6 +57,72 @@ public class MyLinkedList {
         size++; 
         
     }
+    
+    
+    public void add( Person p, int pos ) {
+    	if (pos<0 || pos > size  ) throw new IllegalArgumentException("bad position"); 
+		if (p == null ) throw new IllegalArgumentException("not allowing null"); 
+    	
+        //empty list 
+        if (head == null ){
+            head = new Node (p);
+            size++;
+            return; 
+        }
+        //adding at pos = 0
+        if (pos == 0) {
+        	Node n = new Node (p); 
+        	n.next = head;
+        	head = n;
+        	size++;
+        	return; 
+        }
+				
+		Node current = head; 
+	        
+		for ( int i = 0; i < pos-1  ; i++ ) {
+			current = current.next; 
+		}
+		Node n = new Node (p); 
+		n.next = current.next; 
+        current.next = n; 
+        
+        size++; 
+    	
+    }
+    
+    public Person remove (int pos ) {
+    	if (pos<0 || pos >= size  ) throw new IllegalArgumentException("bad position"); 
+    	
+    	//if (head == null ) return null;    	
+    	//pos = 0 
+    	if (pos == 0 ) {
+    		Node current = head; 
+    		head = head.next; 
+    		size -- ;
+    		return current.person; 
+    	}
+    	
+    	Node prev = head;
+    	Node cur = head.next; 
+    	int at = 1; 
+    	while ( at < pos ) {
+    		prev = prev.next; 
+    		cur = cur.next; 
+    		at++;
+    	}
+    	prev.next = cur.next; 
+    	size--; 
+    	
+    	return cur.person;
+    	
+    }
+    
+    public boolean remove (Person p ) { 
+    	
+    	return false; 
+    }
+    
 	
 	public int size() { 
         int count = 0; 
@@ -79,8 +145,7 @@ public class MyLinkedList {
             current = current.next; 
         }
         return str; 
-        
-        
+          
     }
 	
 	
@@ -88,5 +153,88 @@ public class MyLinkedList {
 	
 	
 	
+	
+	/*
+	
+	public int size () { 
+		//the simplest way assuming that size data field exists 
+		//return size; 
+		
+		//or 
+		Node current = head;
+		int counter = 0;
+		while (current != null ) {
+			counter++;
+			current=current.next; 
+		}
+		return counter; 
+	}
+	
+	public void addBack (Person p ) {
+		if (head == null) {
+			head = new Node(p) ;
+			size++;
+			return;
+		}
+		Node current = head;
+		while (current.next != null ) {
+			current = current.next;
+		}
+		current.next = new Node(p) ;
+		size++;
+	}
+	
+	public void addFront ( Person p ) {
+		Node temp = new Node(p);
+		temp.next = head;
+		head = temp; 
+		size++; 
+	}
+	
+	public Person removeFront( ) {
+		if (head == null )
+			return null;
+		Person temp = head.person;
+		head = head.next;
+		size--; 
+		return temp; 
+		
+	}
+	
+	public Person removeBack( ) {
+		if (head == null ) return null;
+		
+		Person temp = null; 
+		
+		if (head.next == null ) {
+			temp = head.person; 
+			head = null; 
+			return temp;
+		}
+		
+		Node current = head; 
+		while (current.next.next != null ) {
+			current = current.next; 
+		}
+		temp = current.next.person; 
+		current.next = null; 
+		return temp; 
+	}
+	
+	public boolean contains ( Person p ) {
+		
+		Node current = head;
+		
+		while (current != null ) {
+			if (current.person.equals( p )) {
+				return true; 
+			}
+			current = current.next; 
+		}
+		
+		return false; 
+	}
+	
+	*/
 	
 }
